415d42402ded58bccf1d366f3e225a9d401eb1bb
Kevin Coulter <GreenApple76@users.noreply.github.com>
2017-10-04 20:50:43 -0400

    setup ajax call to quote server

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-$(".newquote").on("click", function() {
+// utilizing CORS proxy to work around Cross Origin Resource Sharing issue in the browser since retrieved
+// quote data is on different server than web app 
+$.getJSON("https://cors-anywhere.herokuapp.com/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en", function(json) {
+	$(".quote").html(json.quoteText + "\n<br>--" + json.quoteAuthor)
+		}).fail(function( jqxhr, textStatus, error ) {
+    	var err = textStatus + ", " + error;
+    	$(".quote").html( "OOPS! unable to retrieve data due to error: " + err + ". Please try your request again later.");
+});
 
-	$.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp", function(json) {
-		$(".quote").html(JSON.stringify(json));
-		// $(".author").html(a[0].title);
+// setup event listner to retrieve a new quote when user clicks on the generate new quote button
+$(".newquote").on("click", function() {
+	$.getJSON("https://cors-anywhere.herokuapp.com/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en", function(json) {
+	$(".quote").html(json.quoteText + "\n<br>--" + json.quoteAuthor) }).fail(function( jqxhr, textStatus, error ) {
+    	var err = textStatus + ", " + error;
+    	$(".quote").html( "OOPS! unable to retrieve data due to error: " + err + ". Please try your request again later.");
 	});
 });
